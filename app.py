41b9b888d8ff37c1fe73510d9a541dc5c4843396
longnv <comewithme1200@gmail.com>
2022-12-21 21:43:02 +0700

    restructure project

--- app.py
+++ app.py
@@ -5,38 +5,82 @@
 from flask import Flask
 from flask_jwt_extended import JWTManager
 from flask_restful import Api
-
-from resources.item import Item, ItemList
-from resources.store import Store, StoreList
-from resources.user import UserRegister, User
-from config import postgresqlConfig
+from flask_mysqldb import MySQL
+import uuid
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = postgresqlConfig
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# Setup the Flask-JWT-Extended extension
-app.config["JWT_SECRET_KEY"] = "Dese.Decent.Pups.BOOYO0OST"  # Change this!
+app.config['MYSQL_HOST'] = 'localhost:3306'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'truong_hoc'
 jwt = JWTManager(app)
 api = Api(app)
+mysql = MySQL(app)
 
+# APIs
+@app.route('/sinh_vien', methods=['POST'])
+def them_sinh_vien():
+    ma_sinh_vien = str(uuid.uuid4())
+    ho_ten = request.form['ho_ten']
+    gioi_tinh = request.form['gioi_tinh']
+    ngay_sinh = request.form['ngay_sinh']
+    que_quan = request.form['que_quan']
+    lop = request.form['lop']
+    khoa = request.form['khoa']
 
-@app.before_first_request
-def create_tables():
-    from db import db
-    db.init_app(app)
-    db.create_all()
+      
+    cur = mysql.connection.cursor()
 
+    # data = sinh_vien(
+    #     ma_sinh_vien,
+    #     ho_ten,
+    #     gioi_tinh,
+    #     ngay_sinh,
+    #     que_quan,
+    #     lop,
+    #     khoa
+    #     )
+    
+    cur.execute("INSERT INTO sinh_vien (ma_sinh_vien, ho_ten, gioi_tinh, ngay_sinh, que_quan, lop, khoa) VALUES (%s, %s, %s, %s, %s, %s, %s)", (ma_sinh_vien, ho_ten, gioi_tinh, ngay_sinh, que_quan, lop, khoa))
+    mysql.connection.commit()
 
-# jwt = JWT(app, authenticate, identity)  # Auto Creates /auth endpoint
+    return 'Success'
 
-api.add_resource(Item, '/item/<string:name>')
-api.add_resource(ItemList, '/items')
-api.add_resource(UserRegister, '/register')
-api.add_resource(User, '/user')
-api.add_resource(Store, '/store/<string:name>')
-api.add_resource(StoreList, '/stores')
+@app.route('/sinh_vien/<string:ma_sinh_vien>', methods=['PUT'])
+def sua_sinh_vien():
+    ho_ten = request.form['ho_ten']
+    gioi_tinh = request.form['gioi_tinh']
+    ngay_sinh = request.form['ngay_sinh']
+    que_quan = request.form['que_quan']
+    lop = request.form['lop']
+    khoa = request.form['khoa']  
+
+    cur = mysql.connection.cursor()
+    cur.execute("""
+               UPDATE students
+               SET ho_ten=%s, gioi_tinh=%s, ngay_sinh=%s, que_quan=%s, lop=%s, khoa=%s
+               WHERE ma_sinh_vien=%s
+            """, (ho_ten, gioi_tinh, ngay_sinh, que_quan, lop, khoa, ma_sinh_vien))
+    mysql.connection.commit()
+    return 'success'
+
+@app.route('/sinh_vien', methods=['GET'])
+def get_sinh_vien():  
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT  * FROM sinh_vien")
+    data = cur.fetchall()
+    cur.close()
+
+    return jsontify(data)
+
+@app.route('/sinh_vien/<string:ma_sinh_vien>')
+def xoa_sinh_vien():  
+    cur = mysql.connection.cursor()
+    cur.execute("DELETE FROM sinh_vien WHERE ma_sinh_vien=%s", (ma_sinh_vien))
+    mysql.connection.commit()
+    return 'success'
+
 
 if __name__ == '__main__':
-    # TODO: Add swagger integration
-    app.run(debug=True)  # important to mention debug=True
+    app.run(debug=True)
--- db.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# standard python imports
-
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
