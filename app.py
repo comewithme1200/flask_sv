00b9bcceaac69cc9037b44cfd32419d359f3a42a
longnv <comewithme1200@gmail.com>
2022-12-22 18:31:23 +0700

    add apis for giang vie, hoc phan, chi tiet hoc phan

--- app.py
+++ app.py
@@ -35,15 +35,6 @@ def them_sinh_vien():
       
     cur = mysql.connection.cursor()
 
-    # data = sinh_vien(
-    #     ma_sinh_vien,
-    #     ho_ten,
-    #     gioi_tinh,
-    #     ngay_sinh,
-    #     que_quan,
-    #     lop,
-    #     khoa
-    #     )
     cur.execute("INSERT INTO sinh_vien (ma_sinh_vien, ho_ten, gioi_tinh, ngay_sinh, que_quan, lop, khoa) VALUES (%s, %s, %s, %s, %s, %s, %s)", (ma_sinh_vien, ho_ten, gioi_tinh, ngay_sinh, que_quan, lop, khoa))
     mysql.connection.commit()
 
@@ -89,6 +80,166 @@ def xoa_sinh_vien():
     mysql.connection.commit()
     return 'success'
 
+#giang vien
+@app.route('/giang_vien', methods=['POST'])
+def them_giang_vien():
+    jsonData = request.get_json()
+    ma_giang_vien = str(uuid.uuid4())
+    ho_ten = jsonData['ho_ten']
+    gioi_tinh = jsonData['gioi_tinh']
+    ngay_sinh = jsonData['ngay_sinh']
+    chuc_vu = jsonData['chuc_vu']
+    khoa = jsonData['khoa']  
+
+      
+    cur = mysql.connection.cursor()
+
+    cur.execute("INSERT INTO giang_vien (ma_giang_vien, ho_ten, gioi_tinh, ngay_sinh, khoa, chuc_vu) VALUES (%s, %s, %s, %s, %s, %s)", (ma_giang_vien, ho_ten, gioi_tinh, ngay_sinh, que_quan, lop, khoa))
+    mysql.connection.commit()
+
+    return 'Success'
+
+@app.route('/giang_vien', methods=['PUT'])
+def sua_giang_vien():
+    jsonData = request.get_json()
+    ma_giang_vien = jsonData['ma_giang_vien']
+    ho_ten = jsonData['ho_ten']
+    gioi_tinh = jsonData['gioi_tinh']
+    ngay_sinh = jsonData['ngay_sinh']
+    chuc_vu = jsonData['chuc_vu']
+    khoa = jsonData['khoa']  
+
+    cur = mysql.connection.cursor()
+    cur.execute("""
+               UPDATE giang_vien
+               SET ho_ten=%s, gioi_tinh=%s, ngay_sinh=%s, lop=%s, khoa=%s, chuc_vu=%s
+               WHERE ma_giang_vien=%s
+            """, (ho_ten, gioi_tinh, ngay_sinh, que_quan, lop, khoa, chuc_vu, ma_giang_vien))
+    mysql.connection.commit()
+    return 'success'
+
+@app.route('/giang_vien', methods=['GET'])
+def get_giang_vien():  
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT  * FROM giang_vien")
+    data = cur.fetchall()
+    cur.close()
+
+    return jsonify(data)
+
+
+@app.route('/giang_vien', methods=['DELETE'])
+def xoa_giang_vien():  
+    jsonData = request.get_json()
+    ma_giang_vien = jsonData['ma_giang_vien']
+    cur = mysql.connection.cursor()
+    cur.execute("DELETE FROM giang_vien WHERE ma_giang_vien=%s", (ma_giang_vien))
+    mysql.connection.commit()
+    return 'success'
+
+#hoc phan
+@app.route('/hoc_phan', methods=['POST'])
+def them_hoc_phan():
+    jsonData = request.get_json()
+    ma_hoc_phan = str(uuid.uuid4())
+    ten_hoc_phan = jsonData['ten_hoc_phan']
+
+      
+    cur = mysql.connection.cursor()
+
+    cur.execute("INSERT INTO hoc_phan (ma_hoc_phan, ten_hoc_phan) VALUES (%s, %s)", (ma_hoc_phan, ten_hoc_phan))
+    mysql.connection.commit()
+
+    return 'Success'
+
+@app.route('/hoc_phan', methods=['PUT'])
+def sua_hoc_phan():
+    jsonData = request.get_json()
+    ma_hoc_phan = jsonData['ma_hoc_phan']
+    ten_hoc_phan = jsonData['ten_hoc_phan']
+
+    cur = mysql.connection.cursor()
+    cur.execute("""
+               UPDATE hoc_phan
+               SET ten_hoc_phan=%s
+               WHERE ma_hoc_phan=%s
+            """, (ten_hoc_phan, ma_hoc_phan))
+    mysql.connection.commit()
+    return 'success'
+
+@app.route('/hoc_phan', methods=['GET'])
+def get_hoc_phan():  
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT  * FROM hoc_phan")
+    data = cur.fetchall()
+    cur.close()
+
+    return jsonify(data)
+
+
+@app.route('/hoc_phan', methods=['DELETE'])
+def xoa_hoc_phan():  
+    jsonData = request.get_json()
+    ma_hoc_phan = jsonData['ma_hoc_phan']
+    cur = mysql.connection.cursor()
+    cur.execute("DELETE FROM hoc_phan WHERE ma_hoc_phan=%s", (ma_hoc_phan))
+    mysql.connection.commit()
+    return 'success'
+
+#chi tiet hoc phan
+@app.route('/chi_tiet_hoc_phan', methods=['POST'])
+def them_chi_tiet_hoc_phan():
+    jsonData = request.get_json()
+    ma_chi_tiet = str(uuid.uuid4())
+    ma_hoc_phan = jsonData['ma_hoc_phan']
+    ma_sinh_vien = jsonData['ma_sinh_vien']
+    ma_giang_vien = jsonData['ma_giang_vien']
+    diem = jsonData['diem']
+
+      
+    cur = mysql.connection.cursor()
+
+    cur.execute("INSERT INTO chi_tiet_hoc_phan (ma_chi_tiet, ma_hoc_phan, ma_sinh_vien, ma_giang_vien, diem) VALUES (%s, %s)", (ma_chi_tiet, ma_hoc_phan, ma_sinh_vien, ma_giang_vien, diem))
+    mysql.connection.commit()
+
+    return 'Success'
+
+@app.route('/chi_tiet_hoc_phan', methods=['PUT'])
+def sua_chi_tiet_hoc_phan():
+    jsonData = request.get_json()
+    ma_chi_tiet = jsonData['ma_chi_tiet']
+    ma_hoc_phan = jsonData['ma_hoc_phan']
+    ma_sinh_vien = jsonData['ma_sinh_vien']
+    ma_giang_vien = jsonData['ma_giang_vien']
+    diem = jsonData['diem']
+
+    cur = mysql.connection.cursor()
+    cur.execute("""
+               UPDATE chi_tiet_hoc_phan
+               SET ma_hoc_phan=%s, ma_sinh_vien=%s, ma_giang_vien=%s, diem=%s,
+               WHERE ma_chi_tiet=%s
+            """, (ma_hoc_phan, ma_sinh_vien, ma_giang_vien, diem, ma_chi_tiet))
+    mysql.connection.commit()
+    return 'success'
+
+@app.route('/chi_tiet_hoc_phan', methods=['GET'])
+def get_chi_tiet_hoc_phan():  
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT  * FROM chi_tiet_hoc_phan")
+    data = cur.fetchall()
+    cur.close()
+
+    return jsonify(data)
+
+
+@app.route('/chi_tiet_hoc_phan', methods=['DELETE'])
+def xoa_chi_tiet_hoc_phan():  
+    jsonData = request.get_json()
+    ma_chi_tiet = jsonData['ma_chi_tiet']
+    cur = mysql.connection.cursor()
+    cur.execute("DELETE FROM chi_tiet_hoc_phan WHERE ma_chi_tiet=%s", (ma_chi_tiet))
+    mysql.connection.commit()
+    return 'success'
 
 if __name__ == '__main__':
     app.run(debug=True)
